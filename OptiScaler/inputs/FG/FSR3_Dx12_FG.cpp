95a62fe90f7c9073894518a3a178b76ba2686f5a
cdozdil <cdozdil@gmail.com>
2026-05-08 22:46:44 +0300

    Do not immediately re-activate FG when it's paused

--- OptiScaler/inputs/FG/FSR3_Dx12_FG.cpp
+++ OptiScaler/inputs/FG/FSR3_Dx12_FG.cpp
@@ -707,8 +707,11 @@ static Fsr3::FfxErrorCode hkffxFsr3ConfigureFrameGeneration(void* context, Fsr3:
 
         if (config->frameGenerationEnabled && !fg->IsActive() && Config::Instance()->FGEnabled.value_or_default())
         {
-            fg->Activate();
-            fg->ResetCounters();
+            if (!fg->IsPaused())
+            {
+                fg->Activate();
+                fg->ResetCounters();
+            }
         }
         else if (!config->frameGenerationEnabled && fg->IsActive())
         {
@@ -788,8 +791,11 @@ static Fsr3::FfxErrorCode hkffxSetFrameGenerationConfigToSwapchainDX12(Fsr3::Ffx
 
         if (config->frameGenerationEnabled && !fg->IsActive() && Config::Instance()->FGEnabled.value_or_default())
         {
-            fg->Activate();
-            fg->ResetCounters();
+            if (!fg->IsPaused())
+            {
+                fg->Activate();
+                fg->ResetCounters();
+            }
         }
         else if (!config->frameGenerationEnabled && fg->IsActive())
         {
